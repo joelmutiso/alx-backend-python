8b00babd7ff7cc96449bf8824ee39d58c25b80c7
Joel Mutiso <joelwmutiso@gmail.com>
2025-11-15 21:33:00 +0300

    Configure mock patcher and fixtures for integration tests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -40,9 +40,11 @@ class TestGithubOrgClient(unittest.TestCase):
         Test the _public_repos_url property by mocking the 'org' property.
         """
         known_payload = {"repos_url": "https://api.github.com/my_test/repos"}
-        with patch.object(GithubOrgClient,
-                          'org',
-                          new_callable=PropertyMock) as mock_org:
+        with patch.object(
+            GithubOrgClient,
+            'org',
+            new_callable=PropertyMock
+        ) as mock_org:
             mock_org.return_value = known_payload
             client = GithubOrgClient("test_org")
             result = client._public_repos_url
@@ -59,9 +61,12 @@ class TestGithubOrgClient(unittest.TestCase):
         ]
         expected_repos = ["repo-one", "repo-two"]
         mock_get_json.return_value = test_repos_payload
-        with patch.object(GithubOrgClient,
-                          '_public_repos_url',
-                          new_callable=PropertyMock) as mock_public_repos_url:
+
+        with patch.object(
+            GithubOrgClient,
+            '_public_repos_url',
+            new_callable=PropertyMock
+        ) as mock_public_repos_url:
             fake_url = "https://fake.url/repos"
             mock_public_repos_url.return_value = fake_url
             client = GithubOrgClient("test_org")

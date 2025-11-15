63a3ea3edf1a4b820d5511feda08f6a6202ffe64
Joel Mutiso <joelwmutiso@gmail.com>
2025-11-15 16:49:11 +0300

    Implement validation for GithubOrgClient.has_license

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -83,3 +83,20 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(result, expected_repos)
             mock_public_repos_url.assert_called_once()
             mock_get_json.assert_called_once_with(fake_url)
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self,
+                           repo: Dict,
+                           license_key: str,
+                           expected: bool) -> None:
+        """
+        Test the has_license static method with parameterized inputs.
+        """
+        # 1. ACT
+        result = GithubOrgClient.has_license(repo, license_key)
+
+        # 2. ASSERT
+        self.assertEqual(result, expected)

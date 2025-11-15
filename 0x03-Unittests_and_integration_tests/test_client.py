8490abf115e6a1d30fe6a00b5eefb608ddcb611e
Joel Mutiso <joelwmutiso@gmail.com>
2025-11-15 20:21:47 +0300

    Configure mock patcher and fixtures for integration tests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -21,20 +21,20 @@ class TestGithubOrgClient(unittest.TestCase):
         ("google",),
         ("abc",)
     ])
-    @patch('client.get_json')
-    def test_org(self, org_name: str, mock_get_json: Mock) -> None:
+    def test_org(self, org_name: str) -> None:
         """
         Test that GithubOrgClient.org returns the correct value.
         """
         test_payload = {"name": org_name, "repos_url": "http://some.url"}
-        mock_get_json.return_value = test_payload
-        client = GithubOrgClient(org_name)
+        with patch('client.get_json') as mock_get_json:
+            mock_get_json.return_value = test_payload
+            client = GithubOrgClient(org_name)
 
-        result = client.org
+            result = client.org
 
-        self.assertEqual(result, test_payload)
-        expected_url = f"https://api.github.com/orgs/{org_name}"
-        mock_get_json.assert_called_once_with(expected_url)
+            self.assertEqual(result, test_payload)
+            expected_url = f"https://api.github.com/orgs/{org_name}"
+            mock_get_json.assert_called_once_with(expected_url)
 
     def test_public_repos_url(self) -> None:
         """

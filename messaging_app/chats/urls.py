2bcc713b3f2dfd8611099a97bcfa900259df4d3d
Joel Mutiso <joelwmutiso@gmail.com>
2025-11-16 00:58:38 +0300

    fixed serializers error

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from . import views
 
-router = DefaultRouter()
+# Use routers.DefaultRouter for standard RESTful endpoints
+router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet, basename='user')
 router.register(r'conversations', views.ConversationViewSet, basename='conversation')
 router.register(r'messages', views.MessageViewSet, basename='message')

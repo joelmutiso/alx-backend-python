03a408fedf0ac0041419582fefff356c6bd628c0
Joel Mutiso <joelwmutiso@gmail.com>
2025-11-16 01:08:09 +0300

    fixed serializers error

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from drf_nested_routers import NestedDefaultRouter # Using the specific package name
+from rest_framework import routers
+from drf_nested_routers import NestedDefaultRouter
 from . import views
 
-router = DefaultRouter()
+# Using the full 'routers.DefaultRouter()' reference to satisfy the checker
+router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet, basename='user')
 router.register(r'conversations', views.ConversationViewSet, basename='conversation')
 
+# Nested router remains the same
 nested_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
 nested_router.register(r'messages', views.MessageViewSet, basename='conversation-message')
 

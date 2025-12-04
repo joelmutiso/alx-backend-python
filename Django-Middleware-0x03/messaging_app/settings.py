118758f3f665c93f26af216bc5d8a9a7b0cc0be8
Joel Mutiso <joelwmutiso@gmail.com>
2025-12-05 00:22:11 +0300

    Adding RequestLoggingMiddleware to log user activity

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -58,8 +58,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    # --- custom middleware  ---
     'chats.middleware.RequestLoggingMiddleware',
 ]
 

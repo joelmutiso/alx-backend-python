708d6d0eadad6d17dc54f23325d03e395c95aeba
Joel Mutiso <joelwmutiso@gmail.com>
2025-12-05 14:43:48 +0300

    Adding RestrictAccessByTimeMiddleware to block access outside business hours

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,6 +1,7 @@
 import os
 from datetime import datetime
 from django.conf import settings
+from django.http import HttpResponseForbidden
 
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
@@ -19,5 +20,27 @@ class RequestLoggingMiddleware:
             # Silently fail or log error in a production-safe way
             pass
 
+        response = self.get_response(request)
+        return response
+    
+
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Get the current hour (0-23)
+        current_hour = datetime.now().hour
+
+        # Define allowed hours (9 AM to 6 PM)
+        # 9 AM is 9, 6 PM is 18
+        start_hour = 9
+        end_hour = 18
+
+        # Logic: If current time is NOT between start and end, deny access
+        if not (start_hour <= current_hour < end_hour):
+            return HttpResponseForbidden("Access to the messaging app is restricted between 6 PM and 9 AM.")
+
+        # If time is okay, proceed as normal
         response = self.get_response(request)
         return response
--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
 
     # --- custom middleware  ---
     'chats.middleware.RequestLoggingMiddleware',
+    'chats.middleware.RestrictAccessByTimeMiddleware',
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
